2c7bec520f84c1f738a1d5d702f9508ac8e75f0a
sgajbi <sandeepgajbi@yahoo.com>
2026-06-01 06:20:12 +0800

    Use shared cache helper for workbench providers

--- src/app/services/workbench_service_provider.py
+++ src/app/services/workbench_service_provider.py
@@ -1,6 +1,7 @@
 from app.services.advisor_brief_service import AdvisorBriefService
 from app.services.performance_workspace_service import PerformanceWorkspaceService
 from app.services.risk_workspace_service import RiskWorkspaceService
+from app.services.service_provider_cache import resolve_cached_service
 from app.services.workbench_service import WorkbenchService
 from app.services.workbench_service_factory import (
     build_advisor_brief_service,
@@ -22,38 +23,51 @@ _RISK_WORKSPACE_SERVICE_SIGNATURE: tuple[object, ...] | None = None
 
 def workbench_service() -> WorkbenchService:
     global _WORKBENCH_SERVICE, _WORKBENCH_SERVICE_SIGNATURE
-    signature = workbench_service_signature()
-    if _WORKBENCH_SERVICE is None or _WORKBENCH_SERVICE_SIGNATURE != signature:
-        _WORKBENCH_SERVICE = build_workbench_service()
-        _WORKBENCH_SERVICE_SIGNATURE = signature
-    return _WORKBENCH_SERVICE
+    service, signature = resolve_cached_service(
+        _WORKBENCH_SERVICE,
+        _WORKBENCH_SERVICE_SIGNATURE,
+        workbench_service_signature(),
+        build_workbench_service,
+    )
+    _WORKBENCH_SERVICE = service
+    _WORKBENCH_SERVICE_SIGNATURE = signature
+    return service
 
 
 def performance_workspace_service() -> PerformanceWorkspaceService:
     global _PERFORMANCE_WORKSPACE_SERVICE, _PERFORMANCE_WORKSPACE_SERVICE_SIGNATURE
-    signature = workbench_service_signature()
-    if (
-        _PERFORMANCE_WORKSPACE_SERVICE is None
-        or _PERFORMANCE_WORKSPACE_SERVICE_SIGNATURE != signature
-    ):
-        _PERFORMANCE_WORKSPACE_SERVICE = build_performance_workspace_service(workbench_service())
-        _PERFORMANCE_WORKSPACE_SERVICE_SIGNATURE = signature
-    return _PERFORMANCE_WORKSPACE_SERVICE
+    service, signature = resolve_cached_service(
+        _PERFORMANCE_WORKSPACE_SERVICE,
+        _PERFORMANCE_WORKSPACE_SERVICE_SIGNATURE,
+        workbench_service_signature(),
+        lambda: build_performance_workspace_service(workbench_service()),
+    )
+    _PERFORMANCE_WORKSPACE_SERVICE = service
+    _PERFORMANCE_WORKSPACE_SERVICE_SIGNATURE = signature
+    return service
 
 
 def advisor_brief_service() -> AdvisorBriefService:
     global _ADVISOR_BRIEF_SERVICE, _ADVISOR_BRIEF_SERVICE_SIGNATURE
-    signature = workbench_service_signature()
-    if _ADVISOR_BRIEF_SERVICE is None or _ADVISOR_BRIEF_SERVICE_SIGNATURE != signature:
-        _ADVISOR_BRIEF_SERVICE = build_advisor_brief_service(performance_workspace_service())
-        _ADVISOR_BRIEF_SERVICE_SIGNATURE = signature
-    return _ADVISOR_BRIEF_SERVICE
+    service, signature = resolve_cached_service(
+        _ADVISOR_BRIEF_SERVICE,
+        _ADVISOR_BRIEF_SERVICE_SIGNATURE,
+        workbench_service_signature(),
+        lambda: build_advisor_brief_service(performance_workspace_service()),
+    )
+    _ADVISOR_BRIEF_SERVICE = service
+    _ADVISOR_BRIEF_SERVICE_SIGNATURE = signature
+    return service
 
 
 def risk_workspace_service() -> RiskWorkspaceService:
     global _RISK_WORKSPACE_SERVICE, _RISK_WORKSPACE_SERVICE_SIGNATURE
-    signature = workbench_service_signature()
-    if _RISK_WORKSPACE_SERVICE is None or _RISK_WORKSPACE_SERVICE_SIGNATURE != signature:
-        _RISK_WORKSPACE_SERVICE = build_risk_workspace_service()
-        _RISK_WORKSPACE_SERVICE_SIGNATURE = signature
-    return _RISK_WORKSPACE_SERVICE
+    service, signature = resolve_cached_service(
+        _RISK_WORKSPACE_SERVICE,
+        _RISK_WORKSPACE_SERVICE_SIGNATURE,
+        workbench_service_signature(),
+        build_risk_workspace_service,
+    )
+    _RISK_WORKSPACE_SERVICE = service
+    _RISK_WORKSPACE_SERVICE_SIGNATURE = signature
+    return service
